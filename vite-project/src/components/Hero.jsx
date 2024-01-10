de1e8dedbda62c9ac691dc66dc8802d453a70213
Ethan Gutierrez <Ethan.e.gutierrez@gmail.com>
2024-01-10 04:36:09 -0800

    removing text

--- vite-project/src/components/Hero.jsx
+++ vite-project/src/components/Hero.jsx
@@ -12,11 +12,7 @@ export default function Hero() {
         <h1 className="text-xl font-sans text-gray-700">fresh, local ingredients for a deliciously healthy experience.</h1>
 
         <div className="mt-14 flex flex-row items-center">
-        <button className="bg-green-800 hover:bg-green-700 text-white font-bold py-5 px-7 rounded-md focus:outline-none focus:shadow-outline-blue active:bg-green-700"><h1 className="inter text-xl">Discover Options</h1></button>
-        <div className="justify-center align-middle">
-            <h1 className="ml-4 inter">5,000 users like you</h1>
-            <h1 className="ml-4 inter">have enjoyed our product!</h1>
-        </div>
+            <button className="bg-green-800 hover:bg-green-700 text-white font-bold py-5 px-7 rounded-md focus:outline-none focus:shadow-outline-blue active:bg-green-700"><h1 className="inter text-xl">Discover Options</h1></button>
         </div>
     </div>
     </div>
